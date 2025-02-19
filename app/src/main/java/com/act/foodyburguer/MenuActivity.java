bccf4df1cf47d4881d95f60211a1dcf8e1a35afa
Liliana Martinez <lilianamartinezdiaz2004@gmail.com>
2025-02-19 00:08:00 -0500

    Actividad

---
+++ app/src/main/java/com/act/foodyburguer/MenuActivity.java
@@ -0,0 +1,54 @@
+package com.act.foodyburguer;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Cargar el fragmento de inicio por defecto
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new HomeFragment())
+                    .commit();
+        }
+
+        // Listener para cambiar entre Fragments al hacer clic en el menú
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            if (item.getItemId() == R.id.nav_home) {
+                selectedFragment = new HomeFragment();
+            } else if (item.getItemId() == R.id.nav_ordenes) {
+                selectedFragment = new OrdenesFragment();
+            } else if (item.getItemId() == R.id.nav_perfil) {
+                selectedFragment = new PerfilFragment();
+            }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+            }
+            return true;
+
+
+        });
+    }
+}
---
+++ app/src/main/java/com/act/foodyburguer/OrdenesFragment.java
@@ -0,0 +1,29 @@
+package com.act.foodyburguer;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class OrdenesFragment extends Fragment {
+    private Button btnSolicitarPedido;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_ordenes, container, false);
+
+
+
+    }
+
+}
